6c240bc0f62215b779bc27e6771640709b74db58
hsk <tkdrl319@naver.com>
2024-01-26 16:18:10 +0900

    likeit으로 교체완료

--- Thousand/src/main/java/com/thousand/dto/LikepostDTO.java
+++ Thousand/src/main/java/com/thousand/dto/LikepostDTO.java
@@ -2,15 +2,15 @@ package com.thousand.dto;
 
 
 public class LikepostDTO {
-	private int like;			//좋아요 표시
+	private int likeit;			//좋아요 표시
 	private String id;			//좋아요 누른 아이디 rk
 	private int pno;			//좋아요 누른 게시물 rk
 	
-	public int getLike() {
-		return like;
+	public int getLikeit() {
+		return likeit;
 	}
-	public void setLike(int like) {
-		this.like = like;
+	public void setLikeit(int likeit) {
+		this.likeit = likeit;
 	}
 	public String getId() {
 		return id;
